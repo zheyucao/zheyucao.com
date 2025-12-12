add198c090785ff1531128aaa1f981f309169fd4
angine04 <me@angine.tech>
2025-12-12 10:40:12 +0800

    feat: Implement UnoCSS for styling, update content, dependencies, and refine UI components and animations.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import icon from "astro-icon";
 
 import mdx from "@astrojs/mdx";
 
+import UnoCSS from "@unocss/astro";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), mdx()],
+  integrations: [icon(), mdx(), UnoCSS({ injectReset: true })],
   prefetch: true,
 });
